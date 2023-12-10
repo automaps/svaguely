142fd0471bf38c797b842b79c365aeca91e21aaf
Christian Heider Nielsen <mrnaah@gmail.com>
2023-12-10 16:50:50 +0100

    add docs

--- svaguely/__init__.py
+++ svaguely/__init__.py
@@ -5,4 +5,11 @@ from .data_models import *
 from .metadata import *
 from .parsing import *
 
-__author__ = "Christian Heider Lindbjerg <chen(at)mapspeople.com>"
+PROJECT_NAME = "Pimigranate"
+PROJECT_AUTHOR = "Christian Heider Lindbjerg <chen(at)mapspeople.com>"
+PROJECT_YEAR = 2023
+PROJECT_ORGANISATION = "MapsPeople"
+PROJECT_VERSION = "0.0.1"
+
+__author__ = PROJECT_AUTHOR
+__version__ = PROJECT_VERSION
